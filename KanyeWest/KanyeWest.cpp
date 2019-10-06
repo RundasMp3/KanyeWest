913f44d09ba53b00c9c23216afb356a0c1948661
RAgundez <rolis152@gmail.com>
2019-10-06 13:21:37 -0700

    Defines

--- KanyeWest/KanyeWest.cpp
+++ KanyeWest/KanyeWest.cpp
@@ -8,78 +8,72 @@
 #pragma region defines
 
 #define EFE 0
-#define RDI
-#define RDD
-#define RDS
-#define RDB
-#define RDC
-#define RDIV
-#define RDDV
-#define RDSV
-#define RDBV
-#define RDCV
-#define PRTM
-#define PRTI
-#define PRTD
-#define PRTS
-#define PRTB
-#define PRTC
-#define PRTIV
-#define PRTDV
-#define PRTSV
-#define PRTBV
-#define PRTCV
-#define PUSHI
-#define PUSHD
-#define PUSHS
-#define PUSHB
-#define PUSHC
-#define PUSHKI
-#define PUSHKD
-#define PUSHKS
-#define PUSHKB
-#define PUSHKC
-#define POPI
-#define POPD
-#define POPS
-#define POPB
-#define POPC
-#define POPIV
-#define POPDV
-#define POPSV
-#define POPBV
-#define POPCV
-#define SUM
-#define SUB
-#define MULT
-#define DIV
-#define MOD
-#define AND
-#define OR
-#define XOR
-#define MAX
-#define MIN
-#define INCI
-#define INCD
-#define INCC
-#define DECI
-#define DECD
-#define DECC
-#define BRANCH
-#define CMPLE
-#define CMPL
-#define CMPGE
-#define CMPG
-#define CMPE
-#define CMPNE
-#define BRNCHC
-#define JMPT
-#define JEZ
-#define JNZ
-#define JMZ
-#define JMEZ
-#define JLZ
-#define JLEZ
+#define RDI 1
+#define RDD	2
+#define RDS	3
+#define RDB	4
+#define RDC	5
+#define RDIV 6
+#define RDDV 7
+#define RDSV 8
+#define RDBV 9
+#define RDCV 10
+#define PRTM 11
+#define PRTI 12
+#define PRTD 13
+#define PRTS 14
+#define PRTB 15
+#define PRTC 16
+#define PRTIV 17
+#define PRTDV 18
+#define PRTSV 19
+#define PRTBV 20
+#define PRTCV 21
+#define PUSHI 22
+#define PUSHD 23
+#define PUSHS 24
+#define PUSHB 25
+#define PUSHC 26
+#define PUSHKI 27
+#define PUSHKD 28
+#define PUSHKS 29
+#define PUSHKB 30
+#define PUSHKC 31
+#define POPI 32
+#define POPD 33
+#define POPS 34
+#define POPB 35
+#define POPC 36
+#define POPIV 37
+#define POPDV 38
+#define POPSV 39
+#define POPBV 40
+#define POPCV 41
+#define SUM 42
+#define SUB 43
+#define MULT 44
+#define DIV 45
+#define MOD 46
+#define AND 47
+#define OR 48
+#define XOR 49
+#define MAX 50
+#define MIN 51
+#define INCI 52
+#define INCD 53
+#define INCC 54
+#define DECI 55
+#define DECD 56
+#define DECC 57
+#define BRANCH 58
+#define CMPLE 59
+#define CMPL 60
+#define CMPGE 61
+#define CMPG 62
+#define CMPE 63
+#define CMPNE 64
+#define IDX 64
+#define BRNCHC 66
 #pragma endregion
 
 
@@ -89,6 +83,7 @@ int main(int argc)
 	char buffer[131081];
 	char *sc;
 	char *sd;
+	int pc = 0;//deberia comenzar en 11, creo
 	fp = fopen("file.txt", "w+");
 	char i = fread(buffer, 1, 11, fp);
 	if (!verificarHeader(buffer))
@@ -102,6 +97,20 @@ int main(int argc)
 	i = fread(sc, 1, TSC, fp);
 	int TOS = -1;
 	int PC = 0;
+	while (!sc[pc])//SegmentoDeCodigo[Puntero]!=Null o 0 o final) <---Fixfix
+	{
+		switch (*(sc+pc))//SegmentoDeCodigo[Puntero])
+		{
+		case RDI:
+			scanf("@d", &x);
+			break;
+		case EFE:
+			break;
+		default:
+			printf("Howow! incowect code unu");
+			break;
+		}
+	}
 	if (!loadFile())
 	{
 		running();
@@ -163,7 +172,7 @@ void running()
 		case EFE:
 			break;
 		}
-		case 
+		case
 	}
 }
 
