f64c0358d55635b736cded318f873a7261f64240
Jorge Azahel Mendez Chacon <jorge.mendez@cetys.edu.mx>
2019-10-23 16:14:43 -0700

    Black Skinhead

--- KanyeWest/KanyeWest.cpp
+++ KanyeWest/KanyeWest.cpp
@@ -4,6 +4,11 @@
 #include "pch.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
+#include <fstream>
+#include <stack>
+#include <list>
+#include <array>
 
 #pragma region defines
 
@@ -80,56 +85,98 @@ int verificarHeader(char*);
 int ConvertirShortInt(char*);
 bool loadFile();
 void running(char*, int);
+int compareTo(const char*, char*);
+
+struct st_stack {
+	//char tipo;
+	/*union {
+		short int dir;
+		int entero;
+		double doble;
+		char* apuntador;
+		char caracter;
+	}dato;*/
+	union {
+		uint8_t id;
+		int Int32;
+		unsigned char bytes[4];
+	}u;
+};
+
+
+
 
 int main(int argc)
 {
 	FILE *fp;
-	char buffer[131081];
+	char *buffer = NULL;// [131081];
 	char *sc;
 	char *sd;
-	int pc = 0;//deberia comenzar en 11, creo
-	errno_t err;
+	//errno_t err;
+	st_stack stack; //struct st_stack stack;
+	long filend;
 
-	err = fopen_s(&fp, "test.ye", "rb");
+	fopen_s(&fp, "C:\\test2.ye", "rb");
+	
+	//char i = fread(buffer, 1, 11, fp);
+	fseek(fp, 0, SEEK_END);
+	filend = ftell(fp);
+	rewind(fp);
+	buffer = (char *)malloc((filend + 1) * sizeof(char)); // Enough memory for file + \0
+	fread(buffer, filend, 1, fp); // Read in the entire file
+	fclose(fp); // Close the file
+	buffer[filend] = '\0';
 
-	char i = fread(buffer, 1, 11, fp);
+	//
 	if (!verificarHeader(buffer))
 	{
-		printf("Howow! Bwad headew smh");
+		printf("Howow! Bwad headew smh\n");
 	}
 	else
 	{
-		printf("Gwood!");
+		printf("Gwood!\n");
 	}
-	int TSC =(int)((buffer[7] << 8) | buffer[8]);
-	int TSD = (int)((buffer[9] << 8) | buffer[10]);
-	sc = (char*)malloc(TSC);
-	sd = (char*)malloc(TSD);
-	i = fread(sc, 1, TSC, fp);
-	int TOS = -1;
-	int PC = 0;
-	
-	
 
-	if (!loadFile())
+	//st_stack stack[17];
+
+	int TSC = (int)((buffer[7] << 8) | buffer[8]);
+	//stack->dato = ((buffer[9] << 8) | buffer[10]);
+	sc = (char*)malloc(TSC);
+	sd = (char*)malloc(TSC);
+
+	if (sc == NULL) {
+		printf("Memory not allocated.\n");
+		exit(0);
+	}
+	if (sd == NULL) {
+		printf("Memory not allocated.\n");
+		exit(0);
+	}
+	//i = fread(sc, 1, TSC, fp);
+	int TOS = -1;
+	int PC = 0;//deberia comenzar en 11, creo
+
+
+	for (int i = 0; i < filend; ++i)
 	{
-		running(sc, pc);
+		fread(&stack, sizeof(struct st_stack), 1, fp);
+		printf("Id:  %d\tInt:  %d\tbytes:  %d\n", stack.u.id, stack.u.Int32, stack.u.bytes);//"tipo:  %d\tapuntador:  %d\tcaracter:  %d\tdireccion: %d\tdoble: %d\tentero: %d\n", stack.tipo, stack.dato.apuntador, stack.dato.caracter, stack.dato.dir, stack.dato.doble, stack.dato.entero);
+		printf("bytes:  %d\n", stack.u.bytes[0]);
+		printf("bytes:  %d\n", stack.u.bytes[1]);
+		printf("bytes:  %d\n", stack.u.bytes[2]);
+		printf("bytes:  %d\n", stack.u.bytes[3]);
+	}
+
+	if (loadFile())
+	{
+		running(sc, PC);
 	}
 	else
 	{
 
 	}
 }
-struct st_stack {
-	char tipo;
-	union {
-		short int dir;
-		int entero;
-		double doble;
-		char* apuntador;
-		char caracter;
-	}dato;
-};
+
 
 //11 bytes header
 #define MAX_STACK 100
@@ -150,7 +197,6 @@ bool loadFile()
 
 int verificarHeader(char *s)
 {
-	int compareTo(const char*, char*);
 	return compareTo("ICC2020", s);
 }
 
@@ -166,148 +212,218 @@ int compareTo(const char *a, char *b)
 
 void running(char* sc, int pc)
 {
-	while (!sc[pc])//SegmentoDeCodigo[Puntero]!=Null o 0 o final) <---Fixfix
+	printf("Enhorabuena!\n");
+
+	while (sc[pc] != 0)//SegmentoDeCodigo[Puntero]!=Null o 0 o final) <---Fixfix
 	{
-		switch (*(sc + pc))//SegmentoDeCodigo[Puntero])
+		switch (sc[pc])//*(sc + pc))//SegmentoDeCodigo[Puntero])
 		{
 		case EFE:
 			break;
 		case RDI:
+			pc++;
 			//scanf("@d", &x);
 			break;
 		case RDD:
+			pc++;
 			break;
 		case RDS:
+			pc++;
 			break;
 		case RDB:
+			pc++;
 			break;
 		case RDC:
+			pc++;
 			break;
 		case RDIV:
+			pc++;
 			break;
 		case RDDV:
+			pc++;
 			break;
 		case RDSV:
+			pc++;
 			break;
 		case RDBV:
+			pc++;
 			break;
 		case RDCV:
+			pc++;
 			break;
 		case PRTM:
+			pc++;
 			break;
 		case PRTI:
+			pc++;
 			break;
 		case PRTD:
+			pc++;
 			break;
 		case PRTS:
+			pc++;
 			break;
 		case PRTB:
+			pc++;
 			break;
 		case PRTC:
+			pc++;
 			break;
 		case PRTIV:
+			pc++;
 			break;
 		case PRTDV:
+			pc++;
 			break;
 		case PRTSV:
+			pc++;
 			break;
 		case PRTBV:
+			pc++;
 			break;
 		case PRTCV:
+			pc++;
 			break;
 		case PUSHI:
+			pc++;
 			break;
 		case PUSHD:
+			pc++;
 			break;
 		case PUSHS:
+			pc++;
 			break;
 		case PUSHB:
+			printf("PUSHB\n");
+			pc++;
 			break;
 		case PUSHC:
+			pc++;
 			break;
 		case PUSHKI:
+			pc++;
 			break;
 		case PUSHKD:
+			pc++;
 			break;
 		case PUSHKS:
+			pc++;
 			break;
 		case PUSHKB:
+			pc++;
 			break;
 		case PUSHKC:
+			pc++;
 			break;
 		case POPI:
+			pc++;
 			break;
 		case POPD:
+			pc++;
 			break;
 		case POPS:
+			pc++;
 			break;
 		case POPB:
+			pc++;
 			break;
 		case POPC:
+			pc++;
 			break;
 		case POPIV:
+			pc++;
 			break;
 		case POPDV:
+			pc++;
 			break;
 		case POPSV:
+			pc++;
 			break;
 		case POPBV:
+			pc++;
 			break;
 		case POPCV:
+			pc++;
 			break;
 		case SUM:
+			pc++;
 			break;
 		case SUB:
+			pc++;
 			break;
 		case MULT:
+			pc++;
 			break;
 		case DIV:
+			pc++;
 			break;
 		case MOD:
+			pc++;
 			break;
 		case AND:
+			pc++;
 			break;
 		case OR:
+			pc++;
 			break;
 		case XOR:
+			pc++;
 			break;
 		case MAX:
+			pc++;
 			break;
 		case MIN:
+			pc++;
 			break;
 		case INCI:
+			pc++;
 			break;
 		case INCD:
+			pc++;
 			break;
 		case INCC:
+			pc++;
 			break;
 		case DECI:
+			pc++;
 			break;
 		case DECD:
+			pc++;
 			break;
 		case DECC:
+			pc++;
 			break;
 		case BRANCH:
+			pc++;
 			break;
 		case CMPLE:
+			pc++;
 			break;
 		case CMPL:
+			pc++;
 			break;
 		case CMPGE:
+			pc++;
 			break;
 		case CMPG:
+			pc++;
 			break;
 		case CMPE:
+			pc++;
 			break;
 		case CMPNE:
+			pc++;
 			break;
 		case 65:
+			pc++;
 			break;
 		case BRNCHC:
+			pc++;
 			break;
 
 		default:
-			printf("Howow! incowect code unu");
+			printf("Def\n");
+			pc++;
 			break;
 		}
 	}
